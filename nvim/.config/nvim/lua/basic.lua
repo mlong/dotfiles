bcdf54eda54b883589cdffb4668d77cf92824d01
mlong <mikelong80@gmail.com>
2021-09-25 18:17:41 +0100

    Lua config

---
+++ nvim/.config/nvim/lua/basic.lua
@@ -0,0 +1,55 @@
+vim.g.mapleader = ' '
+vim.g.showcmd = true
+vim.g.ruler = true
+vim.g.backspace = 'indent,eol,start'
+vim.g.hlsearch = true
+vim.g.incsearch = true
+vim.g.ignorecase = true
+vim.g.smartcase = true
+vim.g.backup = false
+vim.g.undodir = '~/.vim/undodir'
+vim.g.undofile = true
+vim.g.scrolloff = 8
+--vim.g.guicursor = true
+
+vim.wo.number = true
+vim.wo.wrap = false
+vim.wo.relativenumber = true
+vim.wo.signcolumn = 'yes'
+
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+vim.bo.swapfile = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.vim/plugged')
+
+Plug 'https://github.com/gruvbox-community/gruvbox'
+Plug 'neovim/nvim-lspconfig'
+Plug 'ThePrimeagen/vim-be-good'
+Plug 'nvim-lua/popup.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'mhinz/vim-rfc'
+
+vim.call('plug#end')
+
+vim.cmd [[
+    filetype plugin indent on
+    syntax enable
+    colorscheme gruvbox
+    set splitbelow
+    set splitright
+    set guicursor=
+]]
+
+local map = vim.api.nvim_set_keymap
+map('i', 'jk', '<ESC>', {noremap = true})
+map('n', '<Leader>ff', ':Telescope find_files<CR>', {noremap = true})
+map('n', '<Leader>fb', ':Telescope buffers<CR>', {noremap = true})
+map('n', '<Leader>fg', ':Telescope live_grep<CR>', {noremap = true})
+map('n', '<Leader>fh', ':Telescope help_tags<CR>', {noremap = true})
