08d4a188ca83c0ebd5b42a9a8108ba6838bdb596
mlong <mikelong80@gmail.com>
2024-03-10 12:07:53 +0000

    kickstart

--- nvim/.config/nvim/after/plugin/telescope.lua
+++
@@ -1,6 +0,0 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "TS: Find Files" })
-vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "TS: Git Files" })
-vim.keymap.set("n", "<leader>ps", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end, { desc = "TS: Project Search" })
